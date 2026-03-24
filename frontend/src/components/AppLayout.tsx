da7b0ed80b448a8efab248acf1e6aa0edcbdd3d7
luis <euluishenrique7@gmail.com>
2026-03-24 11:13:34 -0300

    q

--- frontend/src/components/AppLayout.tsx
+++ frontend/src/components/AppLayout.tsx
@@ -42,7 +42,7 @@ const AppLayout = () => {
           </span>
         </div>
         <nav className="flex-1 px-3 py-2 flex flex-col min-h-0">
-          <div className="space-y-1 overflow-y-auto pr-1 min-h-0 flex-1">
+          <div className="sidebar-scroll space-y-1 overflow-y-auto pr-1 min-h-0 flex-1">
             {mainNavItems.map(({ to, icon: Icon, label }) => (
               <NavLink
                 key={to}
